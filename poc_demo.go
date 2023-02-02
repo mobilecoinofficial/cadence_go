002b174259e3cab23427cbd15eafdf0e18cfd8a5
Ozgur Demir <ozgur@mobilecoin.com>
2023-02-02 12:13:21 -0800

    fix types

--- poc_demo.go
+++ poc_demo.go
@@ -54,10 +54,11 @@ func POCDemoActivity3(ctx context.Context, input string) (string, error) {
 	return fmt.Sprintf("activity3{%s}", input), nil
 }
 
-func POCChildWorkflow1(ctx workflow.Context, input string) (*POCDemoWorkflowResult, error) {
+func POCChildWorkflow1(ctx workflow.Context, input string) (string, error) {
 	log.Printf("POCChildWorkflow1 is triggered with input: %s\n", input)
 
-	res := POCDemoWorkflowResult{}
+	//res := POCDemoWorkflowResult{}
+	var res string
 
 	ao := workflow.ActivityOptions{
 		ScheduleToStartTimeout: time.Minute,
@@ -73,20 +74,17 @@ func POCChildWorkflow1(ctx workflow.Context, input string) (*POCDemoWorkflowResu
 	).Get(ctx, nil)
 	if err != nil {
 		workflow.GetLogger(ctx).Error("POCChildWorkflow1 failed.", zap.Error(err))
-		return &res, err
+		return "", err
 	}
 
-	log.Printf("POCChildWorkflow1 is done for input: %s, result: %s\n", input, res.Output)
-	return &POCDemoWorkflowResult{
-		Output: fmt.Sprintf("%s/%s", input, res.Output),
-	}, nil
-
+	log.Printf("POCChildWorkflow1 is done for input: %s, result: %s\n", input, res)
+	return res, nil
 }
 
-func POCChildWorkflow2(ctx workflow.Context, input string) (*POCDemoWorkflowResult, error) {
+func POCChildWorkflow2(ctx workflow.Context, input string) (string, error) {
 	log.Println("POCChildWorkflow2 is triggered with input: ", input)
 
-	res := POCDemoWorkflowResult{}
+	var res string
 
 	ao := workflow.ActivityOptions{
 		ScheduleToStartTimeout: time.Minute,
@@ -102,20 +100,17 @@ func POCChildWorkflow2(ctx workflow.Context, input string) (*POCDemoWorkflowResu
 	).Get(ctx, nil)
 	if err != nil {
 		workflow.GetLogger(ctx).Error("POCChildWorkflow2 failed.", zap.Error(err))
-		return &res, err
+		return "", err
 	}
 
-	log.Printf("POCChildWorkflow2 is done for input: %s, generated final: %s\n", input, res.Output)
-	return &POCDemoWorkflowResult{
-		Output: res.Output,
-	}, nil
-
+	log.Printf("POCChildWorkflow2 is done for input: %s, generated final: %s\n", input, res)
+	return res, nil
 }
 
-func POCChildWorkflow3(ctx workflow.Context, input string) (*POCDemoWorkflowResult, error) {
+func POCChildWorkflow3(ctx workflow.Context, input string) (string, error) {
 	log.Println("POCChildWorkflow3 is triggered with input: ", input)
 
-	res := POCDemoWorkflowResult{}
+	var res string
 
 	ao := workflow.ActivityOptions{
 		ScheduleToStartTimeout: time.Minute,
@@ -131,16 +126,13 @@ func POCChildWorkflow3(ctx workflow.Context, input string) (*POCDemoWorkflowResu
 	).Get(ctx, nil)
 	if err != nil {
 		workflow.GetLogger(ctx).Error("POCChildWorkflow3 failed.", zap.Error(err))
-		return &res, err
+		return "", err
 	}
 
 	// rand.Intn(max - min) + min
 
-	log.Printf("POCChildWorkflow3 is done for input: %s, result: %s\n", input, res.Output)
-	return &POCDemoWorkflowResult{
-		Output: res.Output,
-	}, nil
-
+	log.Printf("POCChildWorkflow3 is done for input: %s, result: %s\n", input, res)
+	return res, nil
 }
 
 func POCWorkflow(ctx workflow.Context) (*POCDemoWorkflowResult, error) {
@@ -157,7 +149,7 @@ func POCWorkflow(ctx workflow.Context) (*POCDemoWorkflowResult, error) {
 
 	log.Printf("Starting Child Workflow: POCChildWorkflow1\n")
 
-	var result1 POCDemoWorkflowResult
+	var result1 string
 	var err error
 	future1 := workflow.ExecuteChildWorkflow(ctx1, POCChildWorkflow1, "input1")
 	err = future1.Get(ctx, &result1)
@@ -165,7 +157,7 @@ func POCWorkflow(ctx workflow.Context) (*POCDemoWorkflowResult, error) {
 		log.Printf("Error: %s", err)
 		os.Exit(1)
 	}
-	log.Printf("POCChildWorkflow1 ended with result: %s\n", result1.Output)
+	log.Printf("POCChildWorkflow1 ended with result: %s\n", result1)
 
 	cwo2 := workflow.ChildWorkflowOptions{
 		//WorkflowID:                   workflowID,
@@ -175,14 +167,14 @@ func POCWorkflow(ctx workflow.Context) (*POCDemoWorkflowResult, error) {
 
 	log.Printf("Starting Child Workflow: POCChildWorkflow2\n")
 
-	var result2 POCDemoWorkflowResult
-	future2 := workflow.ExecuteChildWorkflow(ctx2, POCChildWorkflow2, result1.Output)
+	var result2 string
+	future2 := workflow.ExecuteChildWorkflow(ctx2, POCChildWorkflow2, result1)
 	err = future2.Get(ctx, &result2)
 	if err != nil {
 		log.Printf("Error: %s", err)
 		os.Exit(1)
 	}
-	log.Printf("POCChildWorkflow2 ended with result: %s\n", result2.Output)
+	log.Printf("POCChildWorkflow2 ended with result: %s\n", result2)
 
 	cwo3 := workflow.ChildWorkflowOptions{
 		//WorkflowID:                   workflowID,
@@ -192,16 +184,16 @@ func POCWorkflow(ctx workflow.Context) (*POCDemoWorkflowResult, error) {
 
 	log.Printf("Starting Child Workflow: POCChildWorkflow3\n")
 
-	var result3 POCDemoWorkflowResult
-	future3 := workflow.ExecuteChildWorkflow(ctx3, POCChildWorkflow3, result2.Output)
+	var result3 string
+	future3 := workflow.ExecuteChildWorkflow(ctx3, POCChildWorkflow3, result2)
 	err = future3.Get(ctx, &result3)
 	if err != nil {
 		log.Printf("Error: %s", err)
 		os.Exit(1)
 	}
-	log.Printf("POCChildWorkflow3 ended with result: %s\n", result3.Output)
+	log.Printf("POCChildWorkflow3 ended with result: %s\n", result3)
 
 	return &POCDemoWorkflowResult{
-		Output: "POCDemoWorkflow is done for username: " + fmt.Sprintf("%s/%s/%s", result1.Output, result2.Output, result3.Output),
+		Output: "POCDemoWorkflow is done for username: " + fmt.Sprintf("%s/%s/%s", result1, result2, result3),
 	}, nil
 }
